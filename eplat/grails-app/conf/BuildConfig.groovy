e3868e522a073102c8fd8ec55c6dee127aeb6c98
obullxl@gmail.com <obullxl@gmail.com@69d292cb-bfd0-545f-0c3b-5230ca4da78a>
2012-12-11 06:55:53 +0000

    增加Maven依赖

--- eplat/grails-app/conf/BuildConfig.groovy
+++ eplat/grails-app/conf/BuildConfig.groovy
@@ -4,15 +4,21 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.dependency.cache.dir = "D:/Data/wps/ivyrepo"
 
 grails.project.dependency.resolution = {
+	
+	pom true
+	
 	// inherit Grails' default dependencies
 	inherits("global") {
 		// specify dependency exclusions here; for example, uncomment this to disable ehcache:
 		// excludes 'ehcache'
 	}
-	log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	
+	log "verbose" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
 	checksums true // Whether to verify checksums on resolve
 
 	repositories {
@@ -31,6 +37,7 @@ grails.project.dependency.resolution = {
 		//mavenRepo "http://download.java.net/maven/2/"
 		//mavenRepo "http://repository.jboss.com/maven2/"
 	}
+	
 	dependencies { // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 		runtime 'mysql:mysql-connector-java:5.1.21' }
 
@@ -49,6 +56,6 @@ grails.project.dependency.resolution = {
 		runtime ":database-migration:1.1"
 
 		compile ':cache:1.0.1'
-		compile ":eclipse-scripts:1.0.6"
+		// compile ":eclipse-scripts:1.0.6"
 	}
 }
