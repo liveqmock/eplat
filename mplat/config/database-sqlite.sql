ee61871c1cf353709afc9e9ceb81260bc31a8ab2
obullxl@gmail.com <obullxl@gmail.com@69d292cb-bfd0-545f-0c3b-5230ca4da78a>
2013-01-06 14:21:20 +0000

    修改为适合MPLAT应用

--- mplat/config/database-sqlite.sql
+++ mplat/config/database-sqlite.sql
@@ -1,19 +1,31 @@
 
-drop table if exists mplat_mutex_ticket;
+DROP TABLE IF EXISTS mplat_mutex_ticket;
 CREATE TABLE mplat_mutex_ticket (
-    name varchar(64) primary key,
-    value bigint,
-    stamp bigint
+    name VARCHAR(64) PRIMARY KEY,
+    value BIGINT,
+    stamp BIGINT
 );
 INSERT INTO mplat_mutex_ticket VALUES('USER-INFO-ID', 0, 0);
 
-drop table if exists mplat_user_info;
+DROP TABLE IF EXISTS mplat_user_info;
 CREATE TABLE mplat_user_info (
-    id bigint primary key,
-    usr_name varchar(128) unique,
-    usr_passwd varchar(256)
+    id BIGINT PRIMARY KEY,
+    usr_name VARCHAR(128) UNIQUE,
+    usr_passwd VARCHAR(256)
 );
 
+DROP TABLE IF EXISTS mplat_exam_info;
+CREATE TABLE mplat_exam_info (
+    id BIGINT PRIMARY KEY,
+    title VARCHAR(1024),
+    rgt_no VARCHAR(64)
+);
 
-
-
+DROP TABLE IF EXISTS mplat_exam_item;
+CREATE TABLE mplat_exam_item(
+    id BIGINT PRIMARY KEY,
+    no VARCHAR(64),
+    exm_id INTEGER,
+    text VARCHAR(1024),
+    UNIQUE(no, exm_id)
+);
