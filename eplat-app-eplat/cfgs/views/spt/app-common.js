6e744900413ab9a1a8c0fdf5bee3c4abf78782bf
obullxl@gmail.com <obullxl@gmail.com@69d292cb-bfd0-545f-0c3b-5230ca4da78a>
2013-02-01 15:41:10 +0000

    增加页面

---
+++ eplat-app-eplat/cfgs/views/spt/app-common.js
@@ -0,0 +1,20 @@
+/**
+ * INIT
+ */
+$(document).ready(function() {
+	$(".item").hover(function(e) {
+		$(this).css("cursor", "pointer");
+		$(this).addClass("item_over");
+		$(this).corner();
+		
+		var img = $("#img_" + $(this).attr("id").split("_")[1]);
+		img.attr("src", img.attr("src").replace("-ico-1", "-ico-2"));
+	}, function(e) {
+		$(this).css("cursor", "auto");
+		$(this).removeClass("item_over");
+		$(this).uncorner();
+		
+		var img = $("#img_" + $(this).attr("id").split("_")[1]);
+		img.attr("src", img.attr("src").replace("-ico-2", "-ico-1"));
+	});
+});
