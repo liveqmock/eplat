90ca0502e58316d6a1190e21d18d96cd6bf4b92d
obullxl@gmail.com <obullxl@gmail.com@69d292cb-bfd0-545f-0c3b-5230ca4da78a>
2013-02-02 06:16:39 +0000

    增加页面

--- eplat-app-eplat/cfgs/views/spt/MainView.js
+++ eplat-app-eplat/cfgs/views/spt/MainView.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
 	$(".item").hover(function(e) {
 		$(this).css("cursor", "pointer");
 		$(this).addClass("item_over");
-		$(this).corner();
+		// $(this).corner();
 		
 		var img = $("#img_" + $(this).attr("id").split("_")[1]);
 		img.attr("src", img.attr("src").replace("-ico-1", "-ico-2"));
 	}, function(e) {
 		$(this).css("cursor", "auto");
 		$(this).removeClass("item_over");
-		$(this).uncorner();
+		// $(this).uncorner();
 		
 		var img = $("#img_" + $(this).attr("id").split("_")[1]);
 		img.attr("src", img.attr("src").replace("-ico-2", "-ico-1"));
